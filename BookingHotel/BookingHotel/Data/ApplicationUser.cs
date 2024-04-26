be5ff55ec96ff2b07369ddd3dbe5a5e6fbd75042
Vadim Matok <matokvadym@gmail.com>
2024-04-26 14:16:16 +0300

    Entities
    Started to do entities

--- BookingHotel/BookingHotel/Data/ApplicationUser.cs
+++ BookingHotel/BookingHotel/Data/ApplicationUser.cs
@@ -1,10 +1,24 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookingHotel.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required, MaxLength(10), RegularExpression(@"^[a-zA-Z]+$"), Unicode(false)]
+        public string FirstName { get; set; }
+
+        [MaxLength(10)]
+        public string? LastName { get; set; }
+
+        [Required, MaxLength(10)]
+        public string RoleName { get; set; }
+
+        [Required, MaxLength(15), RegularExpression(@"[^0-9\+\(\)\s]")]
+        public string ContactNumber { get; set; }
+
     }
 
 }
--- BookingHotel/BookingHotel/Data/Migrations/20240426110225_AddNameRoleToApplicationUser.Designer.cs
+++ BookingHotel/BookingHotel/Data/Migrations/20240426110225_AddNameRoleToApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using BookingHotel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingHotel.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240426110225_AddNameRoleToApplicationUser")]
+    partial class AddNameRoleToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -34,11 +37,6 @@ namespace BookingHotel.Migrations
                         .IsConcurrencyToken()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("ContactNumber")
-                        .IsRequired()
-                        .HasMaxLength(15)
-                        .HasColumnType("nvarchar(15)");
-
                     b.Property<string>("Email")
                         .HasMaxLength(256)
                         .HasColumnType("nvarchar(256)");
@@ -49,8 +47,7 @@ namespace BookingHotel.Migrations
                     b.Property<string>("FirstName")
                         .IsRequired()
                         .HasMaxLength(10)
-                        .IsUnicode(false)
-                        .HasColumnType("varchar(10)");
+                        .HasColumnType("nvarchar(10)");
 
                     b.Property<string>("LastName")
                         .HasMaxLength(10)
