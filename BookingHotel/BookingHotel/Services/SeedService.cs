8cb036d244f52f28f6fee5e03ba3f7372118486f
Vadim Matok <matokvadym@gmail.com>
2024-05-01 23:34:45 +0300

    Seed
    Add something to seed

--- BookingHotel/BookingHotel/Services/SeedService.cs
+++ BookingHotel/BookingHotel/Services/SeedService.cs
@@ -9,13 +9,20 @@ namespace BookingHotel.Services
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IRoleStore<IdentityRole> _roleStore;
+        private readonly IConfiguration _configuration;
         public SeedService(UserManager<ApplicationUser> userManager, IUserStore<ApplicationUser> userStore, 
-            RoleManager<IdentityRole> roleManager, IRoleStore<IdentityRole> roleStore)
+            RoleManager<IdentityRole> roleManager, IRoleStore<IdentityRole> roleStore, IConfiguration configuration)
         {
             _userManager = userManager;
             _userStore = userStore;
             _roleManager = roleManager;
             _roleStore = roleStore;
+            _configuration = configuration;
+        }
+
+        public async Task SeedDatabaseAsync()
+        {
+
         }
     }
 }
